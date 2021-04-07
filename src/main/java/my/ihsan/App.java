d982f757653133131ea674050cf8c7f5ec8e078b
ihsan-akif <ihsan.akif@gmail.com>
2021-04-07 11:13:30 +0800

    first commit

---
+++ src/main/java/my/ihsan/App.java
@@ -0,0 +1,24 @@
+package my.ihsan;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends JFrame
+{
+    public App() {
+        setTitle("Hello World");
+        setSize(688, 488);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+    }
+
+    public static void main( String[] args )
+    {
+        new App();
+    }
+}
